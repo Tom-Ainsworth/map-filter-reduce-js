f2801b347e0e9060c63756f9352836ad14f83407
Tom Ainsworth <tomainsworth94@gmail.com>
2022-03-17 14:46:07 +0000

    Add tutorial code. Complete challenge

---
+++ filter-challenge.js
@@ -0,0 +1,34 @@
+let students = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 84, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 67, english: 87, art: 95},
+    }
+];
+
+const topMaths = students.filter(itm => itm.results.maths >= 90)
+console.log(topMaths)
+
+/** I struggled with this a bit as I was getting confused with how to access the
+ * nested data. It turns out because the data within results is also an object I can use dot
+ * notation to go deeper. As a reminder, the word 'itm' could be anything at all. I used score at first
+ */
--- filter.js
+++ filter.js
@@ -4,6 +4,7 @@
  * command node filter.js in the terminal
  */
 
+// * Filter uses a callback function that MUST return true or false
 
 // Simple Filtering
 const people = [
@@ -21,7 +22,23 @@ const people = [
     },
   ];
   
-  
+// * Filerting works in a similar fashion to the map() method.
+/** Below is an example of how to filter a simple result. 
+ * const 'variableName' = 'array'.filter(itemName => itemName.key 'is greater than or equal to' '21)
+*/
+const overAge = people.filter(person => person.age >= 21);
+console.log(overAge);
+
+// TODO: Filter the array to get just the object with "Paul" in it
+
+const justPaul = people.filter(person => person.name == "Paul")
+console.log(justPaul)
+
+// * The filter method with ALWAYS return an array, even if there is only 1 element
+
+
+
+
   // Complex Filtering
   const students = [
     {
@@ -55,3 +72,51 @@ const people = [
       ]
     },
   ];
+
+//   * Here I'm filtering the students by their 'yrsExperience'. If they have 5 or more years,
+// * they are added to the 'strongSkills' variable
+// * I then return the variable only if it has at least 1 element
+
+  const candidates1 = students.filter(student => {
+      let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5)
+      return strongSkills.length > 0
+  })
+
+  console.log(candidates1)
+  // This returns only the candidates that have a skill with 5+ years experience, but doesn't say which skill
+
+  // * The above is quite complicated, as it has the nested filter. This is how it looks having a seperate filter
+  // * for the strongSkills, and then just being able to call that as and when I like
+
+  // * This variable is useless on its own, but will check an array for whether someone has a strong skill.
+  const hasStrongSkills1 = student => {
+    let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+    return strongSkills.length > 0;
+};
+
+/** I can then use the candidates variable to filter out only students with a strong skill.
+ * But then later could filter for other parameters */
+
+let candidates2 = students.filter(hasStrongSkills1);
+console.log(candidates2)
+
+/** I could go a step further and have another variable called 'has5YearsExp' 
+ * that does the 5+ years check seperately, and then pass that into the hasStrongSkills variable.
+ * This allows me to remove the 'strongSkills variable, and the return statement within 'hasStrongSkills */
+
+const has5YearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills2 = student => student.skills.filter(has5YearsExp).length > 0;
+const candidates3 = students.filter(hasStrongSkills2);
+console.log(candidates3)
+
+// * The point of this is to keep code readable and clear. By isolating each filter,
+// * I can clearly see what variable is responsible for what. With this in mind, I could
+// * also filter potentially by the exact skill. Using the 'has5YearsExp, I could pass the 
+// * student.name == "css" and find out all of the candidates that specifically have 5+ years
+// * exp in css
+
+// ! The result of all of these shows every skill the candidate has, here is how to just show the 
+// ! name of the candidate from the info gathered
+
+const candidateNames = candidates3.map(p => p.name);
+console.log(candidateNames)
